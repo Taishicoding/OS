725f4cbfd54036e8f7623e668f2b30f1b784cb12
Taishi <a1904976@adelaide.edu.au>
2025-08-24 23:07:07 +0930

    PUSH

---
+++ minishell.c
@@ -0,0 +1,208 @@
+/*********************************************************************
+Program : miniShell Version : 1.4
+--------------------------------------------------------------------
+Modified command line interpreter with background process support,
+cd command, perror for system calls, and proper exec failure handling
+--------------------------------------------------------------------
+File : minishell.c
+Compiler/System : gcc/linux
+Author : Taishi Morgan A1904976
+Date : 23/08/25
+********************************************************************/
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+#define NV 20  /* max number of command tokens */
+#define NL 100 /* input buffer size */
+
+char line[NL]; /* command input buffer */
+
+/*Implemented Structure to handle background jobs*/
+struct bg_job {
+    pid_t pid;
+    int job_id;
+    char *command;
+    int active;
+};
+struct bg_job bg_jobs[NV];
+int next_job_id = 1;
+
+/*
+shell prompt
+*/
+void prompt(void) {
+    fflush(stdout);
+}
+/*Initialising the background jobs array to an empty state*/
+void init_bg_jobs(void) {
+    int i;
+    for (i = 0; i < NV; i++) {
+        bg_jobs[i].pid = 0;
+        bg_jobs[i].job_id = 0;
+        bg_jobs[i].command = NULL;
+        bg_jobs[i].active = 0;
+    }
+}
+/*Adding a new background job*/
+int add_bg_job(pid_t pid, char *command) {
+    int i;
+    for (i = 0; i < NV; i++) {
+        if (!bg_jobs[i].active) {
+            bg_jobs[i].pid = pid;
+            bg_jobs[i].job_id = next_job_id++;
+            bg_jobs[i].command = strdup(command);
+            bg_jobs[i].active = 1;
+            return bg_jobs[i].job_id;
+        }
+    }
+    return -1;
+}
+/*Removing the background job*/
+void remove_bg_job(pid_t pid) {
+    int i;
+    for (i = 0; i < NV; i++) {
+        if (bg_jobs[i].active && bg_jobs[i].pid == pid) {
+            printf("[%d]+ Done                 %s\n", bg_jobs[i].job_id, bg_jobs[i].command);
+            free(bg_jobs[i].command);
+            bg_jobs[i].active = 0;
+            bg_jobs[i].pid = 0;
+            bg_jobs[i].command = NULL;
+            break;
+        }
+    }
+}
+/*using WNOHANG, a check is undertaken to find any terminated background processes,
+Returning instantanely provided there is no terminated child*/
+void check_background_processes(void) {
+    int status;
+    pid_t pid;
+    int i;
+    for (i = 0; i < NV; i++) {
+        if (bg_jobs[i].active) {
+            pid = waitpid(bg_jobs[i].pid, &status, WNOHANG);
+            if (pid == -1) {
+                perror("waitpid");
+            } else if (pid > 0) {
+                remove_bg_job(pid);
+            }
+        }
+    }
+}
+/*Reconstructing the command string from the token array*/
+void reconstruct_command(char *v[], char *cmd_buf, int token_count) {
+    int i;
+    cmd_buf[0] = '\0';
+    for (i = 0; i < token_count && v[i] != NULL; i++) {
+        if (i > 0) strcat(cmd_buf, " ");
+        strcat(cmd_buf, v[i]);
+    }
+}
+
+/* argk - number of arguments */
+/* argv - argument vector from command line */
+/* envp - environment pointer */
+int main(int argk, char *argv[], char *envp[]) {
+    int frkRtnVal; /* value returned by fork sys call */
+    char *v[NV];   /* array of pointers to command line tokens */
+    char *sep = " \t\n"; /* command line token separators */
+    int i;         /* parse index */
+    int background; /*Flagging background process detection*/
+    char cmd_string[NL]; /*buffer when reconstructing command*/
+    int job_id; /*Job id for background processses*/
+    /*Initialising background job tracking*/
+    init_bg_jobs();
+    /* prompt for and process one command line at a time */
+    while (1) { /* do Forever */
+        /*before each prompt checking background processes*/
+        check_background_processes();
+
+        prompt();
+        if (fgets(line, NL, stdin) == NULL) {
+            perror("fgets");
+            if (feof(stdin)) {
+                exit(0);
+            }
+            continue;
+        }
+        fflush(stdin);
+
+        // This if() required for gradescope
+        if (feof(stdin)) { /* non-zero on EOF */
+            exit(0);
+        }
+
+        if (line[0] == '#' || line[0] == '\n' || line[0] == '\000'){
+            continue; /* to prompt */
+        }
+
+        v[0] = strtok(line, sep);
+        for (i = 1; i < NV; i++) {
+            v[i] = strtok(NULL, sep);
+            if (v[i] == NULL){
+                break;
+            }
+        }
+        /* assert i is number of tokens + 1 */
+
+        if (v[0] == NULL) {
+            continue;
+        }
+        /*Checking for the progress Ampersand and removing for arg list*/
+        background = 0;
+        if (i > 1 && strcmp(v[i - 1], "&") == 0) {
+            background = 1;
+            v[i - 1] = NULL;
+            i--;
+        }
+        /*reconstructing the command string*/
+        if (background) {
+            reconstruct_command(v, cmd_string, i);
+        }
+
+        if (strcmp(v[0], "cd") == 0) {
+            char *dir = (i > 1) ? v[1] : getenv("HOME");
+            if (dir == NULL) {
+                fprintf(stderr, "cd: HOME not set\n");
+            } else if (chdir(dir) == -1) {
+                perror("cd");
+            }
+            continue;
+        }
+
+        /* fork a child process to exec the command in v[0] */
+        switch (frkRtnVal = fork()) {
+            case -1: /* fork returns error to parent process */
+            {
+                perror("fork");
+                break;
+            }
+            case 0: /* code executed only by child process */
+            {
+                if (execvp(v[0], v) == -1) {
+                    perror("execvp");
+                    exit(1);
+                }
+                break;
+            }
+            default: /* code executed only by parent process */
+            {
+                if (!background) {
+                    if (waitpid(frkRtnVal, NULL, 0) == -1) {
+                        perror("waitpid");
+                    }
+                } else {
+                    job_id = add_bg_job(frkRtnVal, cmd_string);
+                    if (job_id != -1) {
+                        printf("[%d] %d\n", job_id, frkRtnVal);
+                    }
+                }
+                break;
+            }
+        } /* switch */
+    } /* while */
+} /* main */
